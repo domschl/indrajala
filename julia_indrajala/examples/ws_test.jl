d733fed16866e3c3a83951f5eb2b631098079768
Dominik Schlösser <dsc@dosc.net>
2022-09-19 10:49:22 +0200

    ongoing

---
+++ julia_indrajala/examples/ws_test.jl
@@ -0,0 +1,17 @@
+using HTTP.WebSockets
+using MbedTLS
+# using HTTP.IOExtras
+
+println("Starting...")
+sslconfig = MbedTLS.SSLConfig(false)
+# sslconfig = MbedTLS.SSLConfig("/home/dsc/certs/nineveh.pem", "/home/dsc/certs/nineveh-key.pem")
+println("Opening...")
+WebSockets.open("wss://nineveh:23577", sslconfig=sslconfig) do ws
+    for i in 1:30
+        println("Sending...$(i)")
+        send(ws, "Hello")
+        println("Receiving...$(i)")
+        s = receive(ws)
+        println("Received:$(s)")
+    end
+end;
