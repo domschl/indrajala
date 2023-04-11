71fcc7575e18a60ad7e9453b9b43301e28805d6c
Dominik Schlösser <dsc@dosc.net>
2023-04-11 16:33:21 +0200

    Split

--- rust_indrajala/indrajala/src/main.rs
+++ rust_indrajala/indrajala/src/main.rs
@@ -1,10 +1,12 @@
 //use std::time::Duration;
 use async_std::task;
-use paho_mqtt::{AsyncClient, CreateOptionsBuilder}; // , Message};
-use std::fs;
-use toml::Value;
-use std::path::Path;
 use std::env;
+use std::fs;
+use std::path::Path;
+use toml::Value;
+
+mod in_async_mqtt;
+use in_async_mqtt::mq;
 
 fn read_config(toml_file: &str) -> String {
     let toml_str = fs::read_to_string(toml_file).unwrap();
@@ -15,41 +17,6 @@ fn read_config(toml_file: &str) -> String {
     return server_uri;
 }
 
-async fn mq(broker: String) {
-    let create_opts = CreateOptionsBuilder::new()
-        .server_uri(broker)
-        .client_id("rust-mqtt")
-        .finalize();
-
-    let client = AsyncClient::new(create_opts).unwrap_or_else(|err| {
-        println!("Error creating the client: {:?}", err);
-        std::process::exit(1);
-    });
-    client.connect(None).await.unwrap_or_else(|err| {
-        println!("Error connecting: {:?}", err);
-        std::process::exit(1);
-    });
-    client.subscribe("#", 0).await.unwrap();
-
-    loop {
-        client.set_message_callback(|_client, msg| {
-            if let Some(msg) = msg {
-                // get topic
-                let topic = msg.topic();
-                // get payload
-                let payload = msg.payload_str();
-                // check if message was retained:
-                let retained = msg.retained();
-                if retained {
-                    // ignore! println!("Received retained message on topic: {}", topic);
-                } else {
-                    println!("Received message on topic: {} with payload: {}", topic, payload);
-                }
-            }
-        });
-    }
-}
-
 fn main() {
     // read command line arguments
     let args: Vec<String> = env::args().collect();
@@ -57,7 +24,7 @@ fn main() {
     if args.len() < 2 {
         println!("Usage: {} <config_file>", args[0]);
         std::process::exit(1);
-    }   
+    }
     // Check if the file exists
     if !Path::new(&args[1]).exists() {
         println!("File {} does not exist, it should be a TOML file", args[1]);
