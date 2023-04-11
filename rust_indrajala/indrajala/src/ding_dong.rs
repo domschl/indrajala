95c400f1ebe3028c53b2ea2d35dbfa315b233c40
Dominik Schlösser <dsc@dosc.net>
2023-04-11 20:57:20 +0200

    use IndraEvent

--- rust_indrajala/indrajala/src/ding_dong.rs
+++ rust_indrajala/indrajala/src/ding_dong.rs
@@ -1,11 +1,15 @@
-use crate::Indra;
+use crate::IndraEvent;
 use std::time::Duration;
 
-pub async fn ding_dong(sender: async_channel::Sender<Indra>) {
+pub async fn ding_dong(sender: async_channel::Sender<IndraEvent>) {
     loop {
         let a = "ding".to_string();
         let b = "dong".to_string();
-        let dd: Indra = (a, b);
+        let mut dd: IndraEvent;
+        dd = IndraEvent::new();
+        dd.domain = a;
+        dd.data = serde_json::json!(b);
+        //dd.data = serde_json(b);
         async_std::task::sleep(Duration::from_millis(1000)).await;
         sender.send(dd).await.unwrap();
     }
