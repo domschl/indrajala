0dbe7a26c55d9c713f886f5fee1bc1cd21a071ac
Dominik Schlösser <dsc@dosc.net>
2023-04-12 10:22:35 +0200

    module+

--- rust_indrajala/indrajala/src/indra_config.rs
+++ rust_indrajala/indrajala/src/indra_config.rs
@@ -1,4 +1,6 @@
+use std::env;
 use std::fs;
+use std::path::Path;
 use toml::Value;
 
 pub struct IndraConfig {
@@ -8,11 +10,25 @@ pub struct IndraConfig {
 }
 
 impl IndraConfig {
-    pub fn new(config_filename: &str) -> IndraConfig {
-        let toml_str = fs::read_to_string(config_filename).unwrap();
+    pub fn new() -> IndraConfig {
+        // read command line arguments
+        let args: Vec<String> = env::args().collect();
+        // Check if at least one argument is passed
+        if args.len() < 2 {
+            println!("Usage: {} <config_file>", args[0]);
+            std::process::exit(1);
+        }
+        // Check if the file exists
+        let config_filename = args[1].to_string();
+        if !Path::new(&config_filename).exists() {
+            println!("File {} does not exist, it should be a TOML file", args[1]);
+            std::process::exit(1);
+        }
+
+        let toml_str = fs::read_to_string(&config_filename).unwrap();
         let value = toml_str.parse::<Value>().unwrap();
         let indra_config = IndraConfig {
-            config_filename: config_filename.to_string(),
+            config_filename: config_filename,
             toml_str,
             value,
         };
--- rust_indrajala/indrajala/src/main.rs
+++ rust_indrajala/indrajala/src/main.rs
@@ -1,8 +1,6 @@
 //use std::time::Duration;
 use async_channel;
 use async_std::task;
-use std::env;
-use std::path::Path;
 
 mod indra_event;
 use indra_event::IndraEvent;
@@ -23,20 +21,7 @@ async fn router(receiver: async_channel::Receiver<IndraEvent>) {
 }
 
 fn main() {
-    // read command line arguments
-    let args: Vec<String> = env::args().collect();
-    // Check if at least one argument is passed
-    if args.len() < 2 {
-        println!("Usage: {} <config_file>", args[0]);
-        std::process::exit(1);
-    }
-    // Check if the file exists
-    if !Path::new(&args[1]).exists() {
-        println!("File {} does not exist, it should be a TOML file", args[1]);
-        std::process::exit(1);
-    }
-
-    let config = IndraConfig::new(&args[1]);
+    let config = IndraConfig::new();
 
     let (sender, receiver) = async_channel::unbounded::<IndraEvent>();
 
