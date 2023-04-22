80d5e7932c60d171e36c6e4a8b68b1c43af8eb46
Dominik Schlösser <dsc@dosc.net>
2023-04-22 10:29:28 +0200

    useless test code

---
+++ rust_indrajala/plotter/src/main.rs
@@ -0,0 +1,25 @@
+use std::{thread, time};
+use tungstenite::{connect, Message};
+use url::Url;
+
+// Create a websocket connection to a server that sends records
+fn main() {
+    let (mut socket, _response) =
+        connect(Url::parse("ws://localhost:8082").unwrap()).expect("Should work.");
+    println!("Connected to the server");
+
+    let mut f = 1.0;
+    let delta = time::Duration::from_millis(500);
+    socket.write_message(format!("1.0, {}", f).into()).unwrap();
+    println!("sent message");
+    // Loop over the messages from the server
+    while let Ok(msg) = socket.read_message() {
+        // If the message is text, parse it as a record
+        if let Message::Text(text) = msg {
+            println!("Received: {}", text);
+            f += 1.0;
+            thread::sleep(delta);
+            socket.write_message(format!("1.0, {}", f).into()).unwrap();
+        }
+    }
+}
