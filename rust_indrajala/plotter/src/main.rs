a6c9730421aa47a15b20b371b72a0a8fecf381cd
Dominik Schlösser <dsc@dosc.net>
2023-04-22 16:36:32 +0200

    websockets raw

--- rust_indrajala/plotter/src/main.rs
+++ rust_indrajala/plotter/src/main.rs
@@ -1,4 +1,5 @@
-use std::{thread, time};
+//use std::time;
+use chrono::prelude::*;
 use tungstenite::{connect, Message};
 use url::Url;
 
@@ -10,9 +11,12 @@ fn main() {
         connect(Url::parse("ws://localhost:8082").unwrap()).expect("Should work.");
     println!("Connected to the server");
 
-    let mut f = 1.0;
-    let delta = time::Duration::from_millis(500);
-    let ie: IndraEvent = IndraEvent::new();
+    //let delta = time::Duration::from_millis(500);
+    let mut ie: IndraEvent = IndraEvent::new();
+    ie.domain = "$cmd/ws/subs".to_string();
+    ie.from_instance = "ws/plotter".to_string();
+    ie.data_type = "cmd".to_string();
+    ie.data = serde_json::from_str(r#"{"subs":["omu/enviro-master/BME280-1/sensor/#"]}"#).unwrap();
     let ie_txt = ie.to_json().unwrap();
     socket.write_message(ie_txt.into()).unwrap();
     println!("sent message");
@@ -20,10 +24,13 @@ fn main() {
     while let Ok(msg) = socket.read_message() {
         // If the message is text, parse it as a record
         if let Message::Text(text) = msg {
-            println!("Received: {}", text);
-            f += 1.0;
-            thread::sleep(delta);
-            socket.write_message(format!("1.0, {}", f).into()).unwrap();
+            //println!("Received: {}", text);
+            let ier: IndraEvent = IndraEvent::from_json(&text).unwrap();
+            println!(
+                "Temperature at {}: {}",
+                IndraEvent::julian_to_datetime(ier.time_jd_start).with_timezone(&Local),
+                ier.data
+            );
         }
     }
 }
