dfe1a477de44b152dcbcf70d3fab9d34aebb80a7
Dominik Schlösser <dsc@dosc.net>
2023-05-09 08:53:29 +0200

    Housekeeping, starting client tests

--- julia_indrajala/examples/ws_test.jl
+++ julia_indrajala/examples/ws_test.jl
@@ -6,7 +6,7 @@ println("Starting...")
 sslconfig = MbedTLS.SSLConfig(false)
 # sslconfig = MbedTLS.SSLConfig("/home/dsc/certs/nineveh.pem", "/home/dsc/certs/nineveh-key.pem")
 println("Opening...")
-WebSockets.open("wss://nineveh:23577", sslconfig=sslconfig) do ws
+WebSockets.open("ws://localhost:8082", sslconfig=sslconfig) do ws
     for i in 1:30
         println("Sending...$(i)")
         send(ws, "Hello")
