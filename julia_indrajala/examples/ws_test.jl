479246bc4ea0d8df1ee3440a0e40de79393944d9
Dominik Schlösser <dsc@dosc.net>
2023-05-09 10:01:32 +0200

    julia test data

--- julia_indrajala/examples/ws_test.jl
+++ julia_indrajala/examples/ws_test.jl
@@ -1,17 +1,36 @@
 using HTTP.WebSockets
 using MbedTLS
+using UUIDs
+using JSON3
+using Dates
 # using HTTP.IOExtras
+
+struct IndraEvent
+    domain::String
+    from_id::String
+    uuid4::UUID
+    to_scope::String
+    time_jd_start::Float64
+    data_type::String
+    data
+    auth_has::String
+    time_jd_end::Float64
+end;
 
 println("Starting...")
 sslconfig = MbedTLS.SSLConfig(false)
 # sslconfig = MbedTLS.SSLConfig("/home/dsc/certs/nineveh.pem", "/home/dsc/certs/nineveh-key.pem")
 println("Opening...")
 WebSockets.open("ws://localhost:8082", sslconfig=sslconfig) do ws
-    for i in 1:30
+    for i in 1:100
+        jd=datetime2julian(now(UTC))
+        x = rand()
+        msg = IndraEvent("\$event/julia/test", "", UUIDs.uuid4(), "data/test", jd, "number/float", x, "", jd)  
         println("Sending...$(i)")
-        send(ws, "Hello")
-        println("Receiving...$(i)")
-        s = receive(ws)
-        println("Received:$(s)")
+        send(ws, JSON3.write(msg))
+        # println("Receiving...$(i)")
+        # s = receive(ws)
+        # println("Received:$(s)")
+        sleep(.100)
     end
 end;
