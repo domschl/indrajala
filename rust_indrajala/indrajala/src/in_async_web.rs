0e6e7e1b7f1d1d11a2a25a7e586702fac034faf5
Dominik Schlösser <dsc@dosc.net>
2023-05-10 07:34:32 +0200

    Web RESTs

--- rust_indrajala/indrajala/src/in_async_web.rs
+++ rust_indrajala/indrajala/src/in_async_web.rs
@@ -8,7 +8,9 @@ use log::{debug, info, warn};
 
 use tide;
 use tide_rustls::TlsListener;
-use uuid::Uuid;
+use tide::http::{convert::Deserialize};
+//use tide::Request;
+//use uuid::Uuid;
 
 #[derive(Clone)]
 pub struct Web {
@@ -61,79 +63,58 @@ struct WebState {
 }
 
 impl WebState {
-    fn new(sender: async_channel::Sender<IndraEvent>, ie: IndraEvent) -> Self {
-        WebState { sender, ie }
+    fn new(sender: async_channel::Sender<IndraEvent>, ie: IndraEvent  ) -> Self {
+        WebState { sender, ie  }
     }
 }
 
 // Test with:
-// curl --cacert ~/Nextcloud/Security/Certs/ca-root.pem  https://localhost:8081/api/v1/event/full -d '{"domain": "test/ok/XXXXXXXXXXXXXXXX", "from_id":"world-wide-web", "uuid4":"ui324234234234", "to_scope":"to-all-my-friend", "time_start":3.13145, "data_type":"test", "data":"lots-of-data", "auth_hash":"XXX", "time_end":3.2342}'
-// curl --cacert ~/Nextcloud/Security/Certs/ca-root.pem  https://localhost:8081/api/v1/event/simple -d '{"domain": "test/ok/XXXXXXXXXXXXXXXX", "from_instance":"world-wide-web", "from_uuid4":"ui324234234234", "to_scope":"to-all-my-friend", "time_start":"2023-04-15T11:28:00CET", "data_type":"test", "data":"lots-of-data", "auth_hash":"XXX", "time_end":"never"}'
+// curl --cacert ~/Nextcloud/Security/Certs/ca-root.pem  https://localhost:8081/api/v1/event/full -d '{"domain": "test/ok/XXXXXXXXXXXXXXXX", "from_id":"world-wide-web", "uuid4":"ui324234234234", "to_scope":"to-all-my-friend", "time_jd_start":3.13145, "data_type":"test", "data":"lots-of-data", "auth_hash":"XXX", "time_jd_end":3.2342}'
 
 impl AsyncTaskSender for Web {
     async fn async_sender(self, sender: async_channel::Sender<IndraEvent>) {
+        let evpath = self.config.url.clone() + "/event";
         let astate = WebState::new(sender.clone(), IndraEvent::new());
         let mut app = tide::with_state(astate); //new();
         let mut ie: IndraEvent = IndraEvent::new();
         ie.domain = "web".to_string();
         ie.data = serde_json::json!("Indrajala!");
-        let evpath = self.config.url.clone() + "/event/full";
-        let evpathsimple = self.config.url.clone() + "/event/simple";
-        let pt = evpath.as_str();
-        let ptsimple = evpathsimple.as_str();
-        app.at(pt)
+        app.at(evpath.as_str())
             .post(|mut req: tide::Request<WebState>| async move {
-                warn!("POST");
-                let st = req.state().clone();
+                let mut st = req.state().clone();
                 let ie_res: Result<IndraEvent, tide::Error> = req.body_json().await;
                 let mut res: tide::Response = tide::Response::new(tide::StatusCode::Ok);
-                let mut ie: IndraEvent;
                 if ie_res.is_err() {
-                    res.set_body(format!("bad request: {}", ie_res.err().unwrap()));
+                    res.set_body(format!("bad request: {}", &ie_res.as_ref().err().unwrap()));
                     res.set_status(tide::StatusCode::BadRequest);
-                    warn!("bad request");
+                    warn!("bad request {}", ie_res.err().unwrap());
                 } else {
-                    ie = ie_res.unwrap();
-                    if !ie.domain.starts_with("$") {
-                        ie.domain = "$event/".to_string() + ie.domain.as_str();
+                    st.ie = ie_res.unwrap();
+                    if !st.ie.domain.starts_with("$") {
+                        st.ie.domain = "$event/".to_string() + st.ie.domain.as_str();
                     }
-                    debug!("SENDING POST: {:?}", ie);
+                    debug!("SENDING POST: {:?}", st.ie);
                     res.set_body("Ok");
                     res.set_status(tide::StatusCode::Ok);
-                    st.sender.send(ie.clone()).await?;
+                    st.sender.send(st.ie.clone()).await?;
                 }
                 Ok(res)
             });
-        app.at(ptsimple)
-            .post(|mut req: tide::Request<WebState>| async move {
-                let st = req.state().clone();
-                let IndraEvent {
-                    domain,
-                    from_id: _,
-                    uuid4: _,
-                    to_scope: _,
-                    time_jd_start,
-                    data_type: _,
-                    data,
-                    auth_hash: _,
-                    time_jd_end: _,
-                } = req.body_json().await.unwrap();
-                let ie = {
-                    let mut ie = st.ie.clone();
-                    ie.domain = domain;
-                    ie.from_id = "indrajala/web".to_string();
-                    ie.uuid4 = Uuid::new_v4().to_string();
-                    ie.to_scope = "#".to_string();
-                    ie.time_jd_start = time_jd_start;
-                    ie.data_type = "data".to_string();
-                    ie.data = data;
-                    // ie.auth_hash = "".to_string();
-                    // ie.time_jd_end = time_jd_end;
-                    ie
-                };
-                debug!("SENDING POST: {:?}", ie);
-                st.sender.send(ie.clone()).await?;
-                Ok("Indrajala!".to_string())
+        app.at(evpath.as_str())
+            .get(|mut req: tide::Request<WebState>| async move {
+                #[derive(Deserialize)]
+                struct Query {
+                    domain: String,
+                }
+                let mut st = req.state().clone();
+                let q_res: Result<Query, tide::Error> = req.query();
+                if q_res.is_err() {
+                    return Ok(format!("bad request: {} {}",req.url(), &q_res.as_ref().err().unwrap()).to_string());
+                }
+                let q: Query = q_res.unwrap();
+                let domain = q.domain;
+                info!("url {} domain {}", req.url(), domain);
+                Ok(format!("Indrajala! domain={}", domain).to_string())
             });
         if self.config.ssl {
             info!("Web: Listening on {} (ssl)", self.config.address);
