8ae10f7e247fb0c34f3ea0060378d47b44b7062d
Dominik Schlösser <dsc@dosc.net>
2023-05-30 19:34:11 +0200

    cleanup

--- julia_indrajala/examples/ws_test.jl
+++ julia_indrajala/examples/ws_test.jl
@@ -19,9 +19,6 @@ Base.@kwdef mutable struct IndraEvent
     time_jd_end::Any
 end;
 StructTypes.StructType(::Type{IndraEvent}) = StructTypes.Mutable()
-# function new(domain::String, from_id::String, uuid4::UUID, to_scope::String, time_jd_start::Float64, data_type::String, data, auth_has::String, time_jd_end::Float64) :: IndraEvent
-#     return IndraEvent(domain, from_id, uuid4, to_scope, time_jd_start, data_type, data, auth_has, time_jd_end)
-# end;
 
 function indra_subscribe(ws, topics)
     msg = IndraEvent("\$cmd/subs", "ws/julia", UUIDs.uuid4(), "cmd/subs", datetime2julian(now(UTC)), "cmd/subs", JSON3.write(topics), "", datetime2julian(now(UTC)))
@@ -46,26 +43,5 @@ WebSockets.open("ws://localhost:8083") do ws # , sslconfig=sslconfig) do ws
     msg = indra_subscribe(ws, ["\$event/omu/#"])
     for rmsg in ws
         indra_log(ws, "warn", "Received: $(rmsg)")
-        # wmsg = IndraEvent("", "", UUIDs.uuid4(), "", datetime2julian(now(UTC)), "cmd/subs", JSON3.write(["\$event/omu/#"]), "", datetime2julian(now(UTC)))
-        # JSON3.read!(String(rmsg), wmsg)
-        indra_log(ws, "error", "Received: $(rmsg)")
-        # wmsg.domain = "\$log/" * msg.domain
-        # send(ws, JSON3.write(wmsg))
     end
 end;
-
-
-
-#    x=0.0;
-#    for i in 1:10
-#        jd=datetime2julian(now(UTC))
-#        x = x + (rand()-0.5)§10.0;
-#        msg = IndraEvent("\$event/julia/test", "", UUIDs.uuid4(), "data/test", jd, "number/float", x, "", jd)  
-#        println("Sending...$(i)")
-#        send(ws, JSON3.write(msg))
-#        # println("Receiving...$(i)")
-#        # s = receive(ws)
-#        # println("Received:$(s)")
-#        sleep(1.00)
-#    end
-#end;
