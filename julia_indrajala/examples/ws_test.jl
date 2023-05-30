f04cd0c6fd9dc3349f54d75198fd7162ac5d7641
Dominik Schlösser <dsc@dosc.net>
2023-05-30 19:31:44 +0200

    ongoing

--- julia_indrajala/examples/ws_test.jl
+++ julia_indrajala/examples/ws_test.jl
@@ -15,7 +15,7 @@ Base.@kwdef mutable struct IndraEvent
     time_jd_start::Float64
     data_type::String
     data::Any
-    auth_has::Any
+    auth_hash::Any
     time_jd_end::Any
 end;
 StructTypes.StructType(::Type{IndraEvent}) = StructTypes.Mutable()
@@ -24,7 +24,7 @@ StructTypes.StructType(::Type{IndraEvent}) = StructTypes.Mutable()
 # end;
 
 function indra_subscribe(ws, topics)
-    msg = IndraEvent("\$cmd/subs", "ws/julia", UUIDs.uuid4(), "cmd/subs", datetime2julian(now(UTC)), "cmd/subs", JSON3.read(JSON3.write(topics)), "", datetime2julian(now(UTC)))
+    msg = IndraEvent("\$cmd/subs", "ws/julia", UUIDs.uuid4(), "cmd/subs", datetime2julian(now(UTC)), "cmd/subs", JSON3.write(topics), "", datetime2julian(now(UTC)))
     send(ws, JSON3.write(msg))
     return(msg)
 end;
@@ -37,17 +37,18 @@ function indra_log(ws, type, msg)
         msg = IndraEvent("\$log/error", "ws/julia", UUIDs.uuid4(), "log/error", datetime2julian(now(UTC)), "string", JSON3.read(JSON3.write("Invalid log type: $type")), "", datetime2julian(now(UTC)))
     end
     send(ws, JSON3.write(msg))
+    println("Sent: $(msg)")
     return(msg)
 end;
 
 sslconfig = MbedTLS.SSLConfig(false)
-WebSockets.open("ws://localhost:8083", sslconfig=sslconfig) do ws
-    msg = indra_subscribe(ws, ["omu/#"])
+WebSockets.open("ws://localhost:8083") do ws # , sslconfig=sslconfig) do ws
+    msg = indra_subscribe(ws, ["\$event/omu/#"])
     for rmsg in ws
         indra_log(ws, "warn", "Received: $(rmsg)")
-        wmsg = IndraEvent("", "", UUIDs.uuid4(), "", datetime2julian(now(UTC)), "cmd/subs", JSON3.read(JSON3.write(["omu/#"])), "", datetime2julian(now(UTC)))
-        JSON3.read!(String(rmsg), wmsg)
-        indra_log(ws, "error", "Received: $(msg)")
+        # wmsg = IndraEvent("", "", UUIDs.uuid4(), "", datetime2julian(now(UTC)), "cmd/subs", JSON3.write(["\$event/omu/#"]), "", datetime2julian(now(UTC)))
+        # JSON3.read!(String(rmsg), wmsg)
+        indra_log(ws, "error", "Received: $(rmsg)")
         # wmsg.domain = "\$log/" * msg.domain
         # send(ws, JSON3.write(wmsg))
     end
