42cdd8b92b1df6735cdf4c595a8c134c30c5ee6f
Dominik Schlösser <dsc@dosc.net>
2023-06-10 16:46:07 +0200

    Prevent timezone ambiguities, timezones must be defined

--- python_indrajala/indralib/tests/indra_test.py
+++ python_indrajala/indralib/tests/indra_test.py
@@ -1,5 +1,5 @@
 import unittest
-
+from datetime import datetime, timedelta, timezone
 import sys
 import os
 
@@ -35,6 +35,62 @@ class TestIndraEvent(unittest.TestCase):
             self.assertEqual(
                 result, t[2], f"pub:{pub}, sub:{sub} = {t[2]} != ground truth"
             )
+
+
+test_year = 2020
+
+
+class TestFracyearConversion(unittest.TestCase):
+    def assertDateTimeAlmostEqual(self, dt1, dt2, delta_seconds=1):
+        print(dt1, dt2)
+        delta = timedelta(seconds=delta_seconds)
+        diff = abs(dt1 - dt2)
+        self.assertLessEqual(diff, delta)
+
+    def test_fracyear2datetime(self):
+        # Test fracyear2datetime function
+        test_data = [
+            (test_year, datetime(test_year, 1, 1, 0, 0, tzinfo=timezone.utc)),
+            (
+                test_year + 0.1,
+                datetime(test_year, 2, 6, 12, 36, tzinfo=timezone.utc),
+            ),
+            (test_year + 0.2, datetime(test_year, 3, 14, 1, 12, tzinfo=timezone.utc)),
+            (test_year + 0.3, datetime(test_year, 4, 19, 13, 48, tzinfo=timezone.utc)),
+            (test_year + 0.4, datetime(test_year, 5, 26, 2, 24, tzinfo=timezone.utc)),
+            (test_year + 0.5, datetime(test_year, 7, 1, 15, 0, tzinfo=timezone.utc)),
+            (test_year + 0.6, datetime(test_year, 8, 7, 3, 36, tzinfo=timezone.utc)),
+            (test_year + 0.7, datetime(test_year, 9, 12, 16, 12, tzinfo=timezone.utc)),
+            (test_year + 0.8, datetime(test_year, 10, 19, 4, 48, tzinfo=timezone.utc)),
+            (test_year + 0.9, datetime(test_year, 11, 24, 17, 24, tzinfo=timezone.utc)),
+            # (test_year + 1.0, datetime(test_year + 1, 1, 1, 6, 0))
+            # Add more test cases here if needed
+        ]
+
+        for fy, expected_datetime in test_data:
+            result = IndraEvent.fracyear2datetime(fy)
+            self.assertDateTimeAlmostEqual(result, expected_datetime)
+
+    def test_datetime2fracyear(self):
+        # Test datetime2fracyear function
+        test_data = [
+            (datetime(test_year, 1, 1, 0, 0, tzinfo=timezone.utc), test_year + 0.0),
+            (datetime(test_year, 2, 6, 12, 36, tzinfo=timezone.utc), test_year + 0.1),
+            (datetime(test_year, 3, 14, 1, 12, tzinfo=timezone.utc), test_year + 0.2),
+            (datetime(test_year, 4, 19, 13, 48, tzinfo=timezone.utc), test_year + 0.3),
+            (datetime(test_year, 5, 26, 2, 24, tzinfo=timezone.utc), test_year + 0.4),
+            (datetime(test_year, 7, 1, 15, 0, tzinfo=timezone.utc), test_year + 0.5),
+            (datetime(test_year, 8, 7, 3, 36, tzinfo=timezone.utc), test_year + 0.6),
+            (datetime(test_year, 9, 12, 16, 12, tzinfo=timezone.utc), test_year + 0.7),
+            (datetime(test_year, 10, 19, 4, 48, tzinfo=timezone.utc), test_year + 0.8),
+            (datetime(test_year, 11, 24, 17, 24, tzinfo=timezone.utc), test_year + 0.9),
+            # (datetime(test_year + 1, 1, 1, 6, 0), test_year + 1.0)
+            # Add more test cases here if needed
+        ]
+
+        for dt, expected_fracyear in test_data:
+            result = IndraEvent.datetime2fracyear(dt)
+            self.assertAlmostEqual(result, expected_fracyear, delta=0.0001)
 
 
 if __name__ == "__main__":
