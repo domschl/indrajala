f55d1a65ad1e6fa722d9c8cb9176cac31450ec4c
Dominik Schlösser <dsc@dosc.net>
2023-06-10 12:58:08 +0200

    Use standard unittest

--- python_indrajala/indralib/tests/indra_test.py
+++ python_indrajala/indralib/tests/indra_test.py
@@ -1,30 +1,41 @@
+import unittest
+
+import sys
+import os
+
+path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "src")
+print(path)
+sys.path.append(path)
+
 from indra_event import IndraEvent
 
 
-def test_mqcmp():
-    td = [
-        ("abc", "abc", True),
-        ("ab", "abc", False),
-        ("ab", "ab+", True),
-        ("abcd/dfew", "abcd", False),
-        ("ba", "bdc/ds", False),
-        ("abc/def", "abc/+", True),
-        ("abc/def", "asdf/+/asdf", False),
-        ("abc/def/asdf", "abc/+/asdf", True),
-        ("abc/def/ghi", "+/+/+", True),
-        ("abc/def/ghi", "+/+/", False),
-        ("abc/def/ghi", "+/+/+/+", False),
-        ("abc/def/ghi", "+/#", True),
-        ("abc/def/ghi", "+/+/#", True),
-        ("abc/def/ghi", "+/+/+/#", False),
-    ]
-    for t in td:
-        pub = t[0]
-        sub = t[1]
-        if IndraEvent.mqcmp(pub, sub) != t[2]:
-            print(f"pub:{pub}, sub:{sub} = {t[2]}!=ground truth")
-            print("Fix your stuff first!")
-            exit(-1)
+class TestIndraEvent(unittest.TestCase):
+    def test_mqcmp(self):
+        td = [
+            ("abc", "abc", True),
+            ("ab", "abc", False),
+            ("ab", "ab+", True),
+            ("abcd/dfew", "abcd", False),
+            ("ba", "bdc/ds", False),
+            ("abc/def", "abc/+", True),
+            ("abc/def", "asdf/+/asdf", False),
+            ("abc/def/asdf", "abc/+/asdf", True),
+            ("abc/def/ghi", "+/+/+", True),
+            ("abc/def/ghi", "+/+/", False),
+            ("abc/def/ghi", "+/+/+/+", False),
+            ("abc/def/ghi", "+/#", True),
+            ("abc/def/ghi", "+/+/#", True),
+            ("abc/def/ghi", "+/+/+/#", False),
+        ]
+        for t in td:
+            pub = t[0]
+            sub = t[1]
+            result = IndraEvent.mqcmp(pub, sub)
+            self.assertEqual(
+                result, t[2], f"pub:{pub}, sub:{sub} = {t[2]} != ground truth"
+            )
 
 
-test_mqcmp()
+if __name__ == "__main__":
+    unittest.main()
