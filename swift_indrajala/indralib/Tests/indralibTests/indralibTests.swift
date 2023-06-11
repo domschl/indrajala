eeee44dce10911272c1fa6aa12886bd0ae2463c1
Dominik Schlösser <dsc@dosc.net>
2023-06-11 15:22:55 +0200

    Swift mqcmp test with shared test data

--- swift_indrajala/indralib/Tests/indralibTests/indralibTests.swift
+++ swift_indrajala/indralib/Tests/indralibTests/indralibTests.swift
@@ -1,68 +1,68 @@
+import Foundation
 import XCTest
+
 @testable import indralib
 
 final class indralibTests: XCTestCase {
-    func test_mqcmp() {
-        let testCases: [(String, String, Bool)] = [
-            ("abc", "abc", true),
-            ("ab", "abc", false),
-            ("ab", "ab+", true),
-            ("abcd/dfew", "abcd", false),
-            ("ba", "bdc/ds", false),
-            ("abc/def", "abc/+", true),
-            ("abc/def", "asdf/+/asdf", false),
-            ("abc/def/asdf", "abc/+/asdf", true),
-            ("abc/def/ghi", "+/+/+", true),
-            ("abc/def/ghi", "+/+/", false),
-            ("abc/def/ghi", "+/+/+/+", false),
-            ("abc/def/ghi", "+/#", true),
-            ("abc/def/ghi", "+/+/#", true),
-            ("abc/def/ghi", "+/+/+/#", false)
-        ]
-        
-        for testCase in testCases {
-            let pub = testCase.0
-            let sub = testCase.1
-            let expected = testCase.2
-            
-            let result = IndraEvent.mqcmp(pub: pub, sub: sub)
-            
-            XCTAssertEqual(result, expected, "Failed for pub:\(pub), sub:\(sub)")
-        }
+  struct TestData: Codable {
+    let pub: String
+    let sub: String
+    let result: Bool
+  }
+  func test_mqcmp() {
+    // Read mqcmp_data.json file in current directory (no bundle) with test data:
+    // format: [ { "pub": "abc", "sub": "abc", "result": true}, ...]
+    let fileURL = URL(fileURLWithPath: "mqcmp_data.json")
+    do {
+      // Read the JSON data from the file
+      let jsonData = try Data(contentsOf: fileURL)
+      // Decode the JSON data using JSONDecoder
+      let decoder = JSONDecoder()
+      let testCases = try decoder.decode([TestData].self, from: jsonData)
+      // Process the tesst data
+      for testCase in testCases {
+        let result = IndraEvent.mqcmp(pub: testCase.pub, sub: testCase.sub)
+
+        XCTAssertEqual(
+          result, testCase.result, "Failed for pub:\(testCase.pub), sub:\(testCase.sub)")
+      }
+    } catch {
+      print("Error reading JSON file: \(error)")
     }
-    
-    func testIndraEventSerialization() {
-        let event = IndraEvent()
-        event.domain = "test.domain"
-        event.from_id = "test.from_id"
-        event.to_scope = "test.to_scope"
-        event.data_type = "test.data_type"
-        event.data = "{\"key\": \"value\"}"
-        event.auth_hash = "test.auth_hash"
-        
-        // Convert event to JSON string
-        guard let jsonString = event.to_json() else {
-            XCTFail("Failed to convert IndraEvent to JSON")
-            return
-        }
-        
-        // Convert JSON string back to IndraEvent object
-        guard let decodedEvent = IndraEvent.from_json(json_str: jsonString) else {
-            XCTFail("Failed to convert JSON to IndraEvent")
-            return
-        }
-        
-        // Assert that the decoded event matches the original event
-        XCTAssertEqual(decodedEvent.domain, event.domain)
-        XCTAssertEqual(decodedEvent.from_id, event.from_id)
-        XCTAssertEqual(decodedEvent.to_scope, event.to_scope)
-        XCTAssertEqual(decodedEvent.data_type, event.data_type)
-        XCTAssertEqual(decodedEvent.data, event.data)
-        XCTAssertEqual(decodedEvent.auth_hash, event.auth_hash)
+  }
+
+  func testIndraEventSerialization() {
+    let event = IndraEvent()
+    event.domain = "test.domain"
+    event.from_id = "test.from_id"
+    event.to_scope = "test.to_scope"
+    event.data_type = "test.data_type"
+    event.data = "{\"key\": \"value\"}"
+    event.auth_hash = "test.auth_hash"
+
+    // Convert event to JSON string
+    guard let jsonString = event.to_json() else {
+      XCTFail("Failed to convert IndraEvent to JSON")
+      return
     }
-    
-    static var allTests = [
-        ("test_mqcmp", test_mqcmp),
-        ("testIndraEventSerialization", testIndraEventSerialization)
-    ]
+
+    // Convert JSON string back to IndraEvent object
+    guard let decodedEvent = IndraEvent.from_json(json_str: jsonString) else {
+      XCTFail("Failed to convert JSON to IndraEvent")
+      return
+    }
+
+    // Assert that the decoded event matches the original event
+    XCTAssertEqual(decodedEvent.domain, event.domain)
+    XCTAssertEqual(decodedEvent.from_id, event.from_id)
+    XCTAssertEqual(decodedEvent.to_scope, event.to_scope)
+    XCTAssertEqual(decodedEvent.data_type, event.data_type)
+    XCTAssertEqual(decodedEvent.data, event.data)
+    XCTAssertEqual(decodedEvent.auth_hash, event.auth_hash)
+  }
+
+  static var allTests = [
+    ("test_mqcmp", test_mqcmp),
+    ("testIndraEventSerialization", testIndraEventSerialization),
+  ]
 }
--- tests/update_testdata.sh
+++ tests/update_testdata.sh
@@ -2,4 +2,6 @@
 
 echo "Copying mqcmp tes data to all implementations of indralib clients and servers:"
 cp -v domain_test_data/mqcmp_data.json ../python_indrajala/indralib/tests/
+cp -v domain_test_data/mqcmp_data.json ../swift_indrajala/indralib/Tests/
+
 cp -v calendar_test_data/wp_decimal_time_data.json ../python_indrajala/indralib/tests/
