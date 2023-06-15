ba2aa6b100fa4b718cc9e03d109cfda71e3e62aa
Dominik Schlösser <dsc@dosc.net>
2023-06-15 16:31:01 +0200

    not necessary for rust

--- tests/update_testdata.sh
+++ tests/update_testdata.sh
@@ -3,6 +3,5 @@
 echo "Copying mqcmp tes data to all implementations of indralib clients and servers:"
 cp -v domain_test_data/mqcmp_data.json ../python_indrajala/indralib/tests/
 cp -v domain_test_data/mqcmp_data.json ../swift_indrajala/indralib/Tests/
-cp -v domain_test_data/mqcmp_data.json ../rust_indrajala/indra_event/src/
 
 cp -v calendar_test_data/wp_decimal_time_data.json ../python_indrajala/indralib/tests/
