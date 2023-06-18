ff52965708c72bffd50137b20d15b841ef962977
Dominik Schlösser <dsc@dosc.net>
2023-06-18 15:24:18 +0200

    Sophisticated script abortion on error

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,25 @@
+#!/bin/bash
+
+# Terminate script on first error.
+set -e
+
+echo "========================================================================="
+echo "||                    RUST INDRAJALA SERVER TESTS                      ||"
+echo "========================================================================="
 cargo test
 
+echo "========================================================================="
+echo "||                   PYTHON INDRAJALA CLIENT TESTS                     ||"
+echo "========================================================================="
 cd python_indrajala/indralib/tests
 python indra_test.py
 cd ../../..
 
+echo "========================================================================="
+echo "||                   SWIFT INDRAJALA CLIENT TESTS                      ||"
+echo "========================================================================="
 cd swift_indrajala/indralib/Tests
 swift test
 cd ../../..
 
+echo "All done!"
