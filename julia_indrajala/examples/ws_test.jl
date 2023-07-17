87a5c2dc6ee5f2b46d265d0b80497e193d93496c
Dominik Schlösser <dsc@dosc.net>
2023-07-17 13:02:41 +0200

    Fixed for updated IndraEvent

--- julia_indrajala/examples/ws_test.jl
+++ julia_indrajala/examples/ws_test.jl
@@ -11,7 +11,7 @@ Base.@kwdef mutable struct IndraEvent
     domain::String
     from_id::String
     uuid4::UUID
-    parent_uuid4::UUID
+    parent_uuid4::String
     seq_no::Int64
     to_scope::String
     time_jd_start::Float64
@@ -23,7 +23,7 @@ end;
 StructTypes.StructType(::Type{IndraEvent}) = StructTypes.Mutable()
 
 function indra_subscribe(ws, topics)
-    msg = IndraEvent("\$cmd/subs", "ws/julia", UUIDs.uuid4(), None, None, "cmd/subs", datetime2julian(now(UTC)), "cmd/subs", JSON3.write(topics), "", datetime2julian(now(UTC)))
+    msg = IndraEvent("\$cmd/subs", "ws/julia", UUIDs.uuid4(), "", 0, "cmd/subs", datetime2julian(now(UTC)), "cmd/subs", JSON3.write(topics), "", datetime2julian(now(UTC)))
     send(ws, JSON3.write(msg))
     return (msg)
 end;
@@ -31,9 +31,9 @@ end;
 function indra_log(ws, type, msg)
     valid_types = ["error", "warn", "info", "debug"]
     if type in valid_types
-        msg = IndraEvent("\$log/$type", "ws/julia", UUIDs.uuid4(), "log/$type", datetime2julian(now(UTC)), "string", JSON3.read(JSON3.write(msg)), "", datetime2julian(now(UTC)))
+        msg = IndraEvent("\$log/$type", "ws/julia", UUIDs.uuid4(), "", 0, "log/$type", datetime2julian(now(UTC)), "string", JSON3.read(JSON3.write(msg)), "", datetime2julian(now(UTC)))
     else
-        msg = IndraEvent("\$log/error", "ws/julia", UUIDs.uuid4(), "log/error", datetime2julian(now(UTC)), "string", JSON3.read(JSON3.write("Invalid log type: $type")), "", datetime2julian(now(UTC)))
+        msg = IndraEvent("\$log/error", "ws/julia", UUIDs.uuid4(), "", 0, "log/error", datetime2julian(now(UTC)), "string", JSON3.read(JSON3.write("Invalid log type: $type")), "", datetime2julian(now(UTC)))
     end
     send(ws, JSON3.write(msg))
     println("Sent: $(msg)")
@@ -42,6 +42,7 @@ end;
 
 sslconfig = MbedTLS.SSLConfig(false)
 WebSockets.open("ws://localhost:8083") do ws # , sslconfig=sslconfig) do ws
+    indra_log(ws, "info", "Connected to IndraJala from Julia")
     msg = indra_subscribe(ws, ["\$event/omu/#"])
     for rmsg in ws
         indra_log(ws, "warn", "Received: $(rmsg)")
