a360e5f8109062516a1457cc719e54b58fa396a8
Dominik Schlösser <dsc@dosc.net>
2023-07-24 18:56:08 +0200

    mod up

--- lin_install.sh
+++ lin_install.sh
@@ -4,4 +4,5 @@ sudo mkdir /var/lib/indrajala
 sudo mkdir /var/lib/indrajala/tasks
 sudo cp -v target/release/indrajala /usr/bin
 sudo cp -rv python_indrajala/sub_tasks/* /var/lib/indrajala/tasks
+sudo cp -rv python_indrajala/indralib /var/lib/indrajala/tasks
 sudo chown -R indrajala:indrajala /var/lib/indrajala
