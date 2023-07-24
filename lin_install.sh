a7672f378501eb275a5f0fec13a317505f7ec2f3
Dominik Schlösser <dsc@dosc.net>
2023-07-24 18:15:33 +0200

    linux min-inst

---
+++ lin_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo mkdir /var/lib/indrajala
+sudo mkdir /var/lib/indrajala/tasks
+sudo cp -v target/release/indrajala /usr/bin
+sudo cp -rv python_indrajala/sub_tasks/* /var/lib/indrajala/tasks
+sudo chown -R indrajala:indrajala /var/lib/indrajala
