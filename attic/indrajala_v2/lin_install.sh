95f48fe1bffe8a8bc20a8f6bed0e624408da3ac2
Dominik Schlösser <dominik.schloesser@gmail.com>
2024-02-07 08:22:02 +0100

    Cleaned out the attic

--- attic/indrajala_v2/lin_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cargo build --release
-sudo mkdir /var/lib/indrajala
-sudo mkdir /var/lib/indrajala/tasks
-sudo cp -v target/release/indrajala /usr/bin
-sudo cp -rv python_indrajala/sub_tasks/* /var/lib/indrajala/tasks
-sudo cp -rv python_indrajala/indralib /var/lib/indrajala/tasks
-sudo chown -R indrajala:indrajala /var/lib/indrajala
--- attic/python_indrajala_v1/publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm dist/*
-python -m build
-twine upload dist/*
-
