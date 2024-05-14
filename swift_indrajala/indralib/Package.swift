421317e6c8cbd7fa82f67b733f7fb8e7a7b075af
Dominik Schlösser <dominik.schloesser@gmail.com>
2024-05-14 21:27:01 +0200

    incomplete

---
+++ swift_indrajala/indralib/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "indralib",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "indralib",
+            targets: ["indralib"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "indralib"),
+        .testTarget(
+            name: "indralibTests",
+            dependencies: ["indralib"]),
+    ]
+)
