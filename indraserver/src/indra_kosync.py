4b378adfbaa9d9fd85f3ccb0a565ba68a93004c7
Dominik Schlösser <dominik.schloesser@gmail.com>
2024-07-17 21:07:39 +0200

    re-translation md5 to filenames

--- indraserver/src/indra_kosync.py
+++ indraserver/src/indra_kosync.py
@@ -5,6 +5,7 @@ from aiohttp import web
 import json
 import os
 import time
+import hashlib
 
 from indralib.indra_event import IndraEvent  # type: ignore
 from indra_serverlib import IndraProcessCore
@@ -43,6 +44,8 @@ class IndraProcess(IndraProcessCore):
         if os.path.exists(self.state_file):
             with open(self.state_file, "r") as f:
                 self.reading_state = json.load(f)
+        self.library_state_filename = self.config_data["library_state_file"]
+        self._update_library_state()
         self.tls = False
         if self.config_data["tls"] is True:
             if os.path.exists(self.config_data["private_key"]) is False:
@@ -76,6 +79,23 @@ class IndraProcess(IndraProcessCore):
                 self.log.error(f"Cannot create cert chain: {e}, not using TLS")
                 self.tls = False
         asyncio.create_task(self.async_web_agent())
+
+    def _update_library_state(self):
+        self.md5_to_filename = {}
+        self.library_state = {}
+        if os.path.exists(self.library_state_filename):
+            with open(self.library_state_filename, "r") as f:
+                self.library_state = json.load(f)
+        for document in self.library_state:
+            if 'uuid' in document:
+                uuid = document['uuid']
+            else:
+                uuid = "NONE"
+            if 'docs' in self.library_state[document]:
+                for doc in self.library_state[document]['docs']:
+                    fn = doc['name']
+                    md5 = hashlib.md5(fn.encode('utf-8')).hexdigest()
+                    self.md5_to_filename[md5] = {'filename': fn, 'uuid': uuid}
 
     async def async_web_agent(self):
         runner = web.AppRunner(self.app)
@@ -115,11 +135,13 @@ class IndraProcess(IndraProcessCore):
     async def user_create_handler(self, request):
         data = await request.json()
         if "username" not in data:
+            self.log.error("No username in request")    
             return web.json_response({"error": "No username in request"}, status=400)
-        self.log.debug(f"User create request: {data["username"]}")
         if data["username"] in self.reading_state:
+            self.log.error(f"User {data["username"]} already exists")
             return web.json_response({"error": "User already exists"}, status=400)
         self.reading_state[data["username"]] = {"password": data["password"], "documents": {}}
+        self.log.info(f"User create request: {data["username"]}")
         self._save_state()
         return web.json_response({"username": data["username"]}, status=201)
 
@@ -127,13 +149,18 @@ class IndraProcess(IndraProcessCore):
         headers = request.headers
         self.log.debug(f"User auth request: {headers}")
         if "x-auth-user" not in headers:
+            self.log.error("No username in request")
             return web.json_response({"error": "No username in request"}, status=400)
         if "x-auth-key" not in headers:
+            self.log.error("No password in request")
             return web.json_response({"error": "No password in request"}, status=400)
         if headers["x-auth-user"] not in self.reading_state:
+            self.log.error(f"User {headers["x-auth-user"]} not found")
             return web.json_response({"error": "Authentication failure (UNE)"}, status=400)
         if headers["x-auth-key"] != self.reading_state[headers["x-auth-user"]]["password"]:
+            self.log.error(f"User {headers["x-auth-user"]} password mismatch")
             return web.json_response({"error": "Authentication failure (PWF)"}, status=401)
+        self.log.info(f"User {headers["x-auth-user"]} authenticated")
         return None
 
     async def user_auth_handler(self, request):
@@ -149,6 +176,11 @@ class IndraProcess(IndraProcessCore):
         document = request.match_info["document"]
         self.log.debug(f"Syncs get progress request: {document}")
         user_info = self.reading_state[request.headers["x-auth-user"]]
+        if document not in self.md5_to_filename:
+            self.log.warning(f"Document {document} not found in library")
+        else:
+            filename = self.md5_to_filename[document]['filename']
+            self.log.info(f"Document {document} is {filename}") 
         if document not in user_info["documents"]:
             progress = {
                 "document": document,
@@ -158,6 +190,7 @@ class IndraProcess(IndraProcessCore):
             }
             self.reading_state[request.headers["x-auth-user"]]["documents"][document] = progress
             self._save_state()
+
         else:
             progress = user_info["documents"][document]
         return web.json_response(progress)
@@ -168,7 +201,13 @@ class IndraProcess(IndraProcessCore):
             return ret
         data = await request.json()
         self.log.debug(f"Syncs put progress request: {data}")
-        self.reading_state[request.headers["x-auth-user"]]["documents"][data["document"]] = data
+        document = data["document"]
+        self.reading_state[request.headers["x-auth-user"]]["documents"][document] = data
+        if document not in self.md5_to_filename:
+            self.log.warning(f"Document {document} not found in library")
+        else:
+            filename = self.md5_to_filename[document]['filename']
+            self.log.info(f"Document {document} is {filename}") 
         self._save_state()
         return web.json_response({"document": data["document"], "timestamp": time.time()}, status=200)
 
