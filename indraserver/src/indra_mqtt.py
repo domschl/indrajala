a4c7fdf5aaeaf0ed03dba2dad3121fd26e5ba9fb
Dominik Schlösser <dominik.schloesser@gmail.com>
2024-08-20 17:35:09 +0200

    again names

--- indraserver/src/indra_mqtt.py
+++ indraserver/src/indra_mqtt.py
@@ -179,10 +179,10 @@ class IndraProcess(IndraProcessCore):
     def ha(self, topic, message):
         self.log.debug(f"inbound-parser-ha: {topic}, {message}")
         topic_list = [
-            ("hastates/sensor/klima_balkon_actual_temperature/state", "temperature", "climate", "home-balkon-ha", "number/float/temperature/celsius"),
-            ("hastates/sensor/klima_balkon_humidity/state", "humidity", "climate", "home-balkon-ha", "number/float/humidity/percentage"),
-            ("hastates/sensor/klima_nordseite_temperature/state", "temperature", "climate", "home-nordseite-ha", "number/float/temperature/celsius"),
-            ("hastates/sensor/klima_nordseite_humidity/state", "humidity", "climate", "home-nordseite-ha", "number/float/humidity/percentage"),
+            ("hastates/sensor/klima_balkon_actual_temperature/state", "temperature", "climate", "home_balkon_ha", "number/float/temperature/celsius"),
+            ("hastates/sensor/klima_balkon_humidity/state", "humidity", "climate", "home_balkon_ha", "number/float/humidity/percentage"),
+            ("hastates/sensor/klima_nordseite_temperature/state", "temperature", "climate", "home_nordseite_ha", "number/float/temperature/celsius"),
+            ("hastates/sensor/klima_nordseite_humidity/state", "humidity", "climate", "home_nordseite_ha", "number/float/humidity/percentage"),
         ]
         for topic in topic_list:
             if IndraEvent.mqcmp(topic[0], topic) is True:
