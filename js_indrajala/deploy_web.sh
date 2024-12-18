985771b616da2a0bd018e6b42878bc59816640d2
Dominik Schlösser <dominik.schloesser@gmail.com>
2024-12-18 13:42:53 +0100

    partial

--- js_indrajala/deploy_web.sh
+++ js_indrajala/deploy_web.sh
@@ -1,1 +1,14 @@
-rsync -avh --exclude "deploy_web.sh" ./ ../web/ --delete
+#!/bin/bash
+
+# Check, if executable `tomlq` is available, else exit:
+if ! command -v tomlq &> /dev/null; then
+    echo "tomlq could not be found, 'pip install tomlq' or similar."
+    exit 1
+fi
+
+data_directory=$(tomlq -r '.indrajala.data_directory' ../config/indrajala.toml)
+# expand tilde:
+data_directory="${data_directory/#\~/$HOME}"
+echo "Using data_directory: >$data_directory<"
+
+rsync -avh --exclude "deploy_web.sh" ./ $data_directory/web/ --delete
