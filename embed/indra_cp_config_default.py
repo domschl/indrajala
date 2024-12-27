e4b4966b4ebdb8d26bce901d92fc34832a720177
Dominik Schlösser <dominik.schloesser@gmail.com>
2024-12-27 15:54:53 +0100

    san

---
+++ embed/indra_cp_config_default.py
@@ -0,0 +1,9 @@
+ssid = "SSID"
+password = "pwd"
+indraserver_url = "https://host:8080"
+device_hostname = "rp2040-name"
+# Set cert_authority to None if not used
+cert_authority = """-----BEGIN CERTIFICATE-----
+...your certificate authority here...
+-----END CERTIFICATE-----"""
+
